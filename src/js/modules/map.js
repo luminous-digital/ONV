85d993ea1a662975921daaab4d84f176d0b2e070
Michal T <michal.trykoszko@pagepro.pl>
2019-05-30 11:24:05 +0200

    fix map

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -11,10 +11,7 @@ const Map = {
         const pinModal = pin.querySelector('.js-map-modal')
         const modalClose = pin.querySelector('.js-map-modal-close')
         pin.addEventListener('click', e => {
-          if (e.target !== e.currentTarget) {
-            // prevent opening again when clicking close
-            return
-          }
+          if (e.target !== e.currentTarget) { return }
           mapPins.forEach(pin => {
             pin.querySelector('.js-map-modal').classList.remove('is-active')
           })
@@ -55,27 +52,42 @@ const Map = {
       const mapWrapper = navWrapper.previousElementSibling
       const mapPins = Array.from(mapWrapper.querySelectorAll('.js-map-pin'))
       const mapFilters = Array.from(navWrapper.querySelectorAll('.js-map-filter'))
+      const clearAllBtn = navWrapper.querySelector('.js-map-clear-all')
       mapFilters.forEach(filter => {
         const filterOptions = Array.from(filter.querySelectorAll('.c-dropdown__option'))
+        const filterName = filter.querySelector('.js-filter-name')
         const filterChosenOptionText = filter.querySelector('.js-chosen-option')
         const filterType = filter.dataset.filterType
         filterOptions.forEach(option => {
           option.addEventListener('click', e => {
-            mapFilters.forEach(filter => {
-              filter.querySelector('.js-chosen-option').innerHTML = ''
-              filter.dataset.chosenOption = ''
-            })
-            mapPins.forEach(pin => {
-              pin.style.opacity = 1
-            })
             const chosenOption = e.currentTarget.querySelector('.c-label').innerText.trim()
             filter.dataset.chosenOption = chosenOption
-            filterChosenOptionText.innerHTML = `&nbsp;(${chosenOption})`
+            filterChosenOptionText.innerHTML = chosenOption
+            filterName.classList.remove('is-visible')
+            filterChosenOptionText.classList.add('is-visible')
             this.mapFilterObject[filterType] = chosenOption
             window.setTimeout(() => {
               this.filterMap(mapPins)
+              clearAllBtn.classList.add('is-visible')
             }, 500)
           })
+        })
+      })
+      clearAllBtn.addEventListener('click', e => {
+        mapFilters.forEach(filter => {
+          const filterName = filter.querySelector('.js-filter-name')
+          const filterChosenOptionText = filter.querySelector('.js-chosen-option')
+          const pins = mapWrapper.querySelectorAll('.js-map-pin')
+          this.mapFilterObject = {}
+          filter.dataset.chosenOption = ''
+          filterName.classList.add('is-visible')
+          filterChosenOptionText.classList.remove('is-visible')
+          window.setTimeout(() => {
+            clearAllBtn.classList.remove('is-visible')
+            Array.from(pins).forEach(pin => {
+              pin.style.opacity = 1
+            })
+          }, 500)
         })
       })
     }
