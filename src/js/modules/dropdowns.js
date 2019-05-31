869aeb5f14e742c45b4d6057e30432c83273883b
Michal T <michal.trykoszko@pagepro.pl>
2019-05-31 14:06:27 +0200

    fix operations subpages tabs

--- src/js/modules/dropdowns.js
+++ src/js/modules/dropdowns.js
@@ -1,6 +1,10 @@
 const Dropdowns = {
   init: function () {
     this.initDropdowns()
+    const windowWidth = window.innerWidth
+    if (windowWidth < 1200) {
+      this.initMobileDropdowns()
+    }
   },
   initDropdowns: function () {
     const dropdownTogglers = document.querySelectorAll('.js-dropdown-toggle')
@@ -8,6 +12,17 @@ const Dropdowns = {
       if (window.innerWidth < 768) {
         dropdownTogglers[0].parentNode.classList.add('is-open')
       }
+      Array.from(dropdownTogglers).forEach(toggler => {
+        toggler.addEventListener('click', e => {
+          const dropdown = e.currentTarget.parentNode
+          dropdown.classList.toggle('is-open')
+        })
+      })
+    }
+  },
+  initMobileDropdowns: function () {
+    const dropdownTogglers = document.querySelectorAll('.js-dropdown-toggle-alt')
+    if (dropdownTogglers.length) {
       Array.from(dropdownTogglers).forEach(toggler => {
         toggler.addEventListener('click', e => {
           const dropdown = e.currentTarget.parentNode
