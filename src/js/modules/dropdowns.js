17d02519cd4a67ad6f661c9408f2d21207edb564
Michal T <michal.trykoszko@pagepro.pl>
2019-06-12 17:04:51 +0200

    fix map filters, fix map on second tab

--- src/js/modules/dropdowns.js
+++ src/js/modules/dropdowns.js
@@ -9,9 +9,9 @@ const Dropdowns = {
   initDropdowns: function () {
     const dropdownTogglers = document.querySelectorAll('.js-dropdown-toggle')
     if (dropdownTogglers.length) {
-      if (window.innerWidth < 768) {
-        dropdownTogglers[0].parentNode.classList.add('is-open')
-      }
+      // if (window.innerWidth < 768) {
+      //   dropdownTogglers[0].parentNode.classList.add('is-open')
+      // }
       Array.from(dropdownTogglers).forEach(toggler => {
         toggler.addEventListener('click', e => {
           const dropdown = e.currentTarget.parentNode
