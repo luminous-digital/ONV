482bfc7824bfbcbae7fd5ea8ae6a67bdf3fe6258
Chris Lojniewski <krzysztof.lojniewski@pagepro.pl>
2020-02-11 18:22:04 +0100

    Update video-lazy-loading.js to support multiple instances

--- src/js/modules/video-lazy-loading.js
+++ src/js/modules/video-lazy-loading.js
@@ -5,29 +5,34 @@ const VideoLazyLoading = {
     const $videoWrapper = $('.js-video-lazy-load')
 
     if (!$videoWrapper.length) return
+    
+    $videoWrapper.each(function () {
+        const $wrap = $(this)
+        const videoUrl = $wrap.data('embed')
+        const [, videoId] = videoUrl.split('/embed/')
 
-    const videoUrl = $videoWrapper.data('embed')
-    const [, videoId] = videoUrl.split('/embed/')
+        const source = `https://img.youtube.com/vi/${videoId}/sddefault.jpg`
 
-    const source = `https://img.youtube.com/vi/${videoId}/sddefault.jpg`
+        $wrap.append(
+            $('<img/>', {
+                src: source
+            })
+        )
 
-    $videoWrapper.append(
-      $('<img/>', {
-        src: source
-      })
-    )
-
-    $videoWrapper.on('click', function (e) {
-      e.preventDefault()
-      const videoSource = `https://www.youtube.com/embed/${videoId}?rel=0&showinfo=0&autoplay=1`
-      $(this).html('')
-      $(this).append(
-        $('<iframe/>', {
-          frameborder: 0,
-          allowfullscreen: true,
-          src: videoSource
+        $wrap.on('click', function (e) {
+            e.preventDefault()
+            const videoSource = `https://www.youtube.com/embed/${videoId}?rel=0&showinfo=0&autoplay=1`
+            $(this).html('')
+            $(this).append(
+                $('<iframe/>', {
+                    frameborder: 0,
+                    allowfullscreen: true,
+                    src: videoSource
+                })
+            )
         })
-      )
+    });
+    
 
       // const iframe = $('iframe')[0]
 
